0e4286e63fe99f25f0efa8e218714a8f98e576b2
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-09-08 14:16:53 +0200

    12 objects

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,8 @@
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (let i = 0; i < lines.length; i++) {
+  if (lines[i].includes("12")) {
+    lines[i] = lines[i].replace("12", "89");
+  }
+  console.log(lines[i]);
+}
